12ecc70b4e0eb2f883c4571847af7c823885e33e
Luish2o <149203184+Luish2o@users.noreply.github.com>
2024-11-18 22:46:58 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/Controller/ContentController.java
@@ -0,0 +1,24 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContentController {
+	
+    @GetMapping("/req/login")
+    public String login(){
+        return "login";
+    }
+    
+    @GetMapping("/req/signup")
+    public String signup(){
+        return "signup";
+    }
+    @GetMapping("/almacen")
+    public String home(){
+        return "almacen";
+    }
+    
+    
+}
