a100d23a8aa1d5da507c24aaf036d5928abde9f7
Luish2o <149203184+Luish2o@users.noreply.github.com>
2024-11-18 22:55:14 -0500

    Delete src directory

--- src/main/resources/static/js/script.js
+++
@@ -1,7 +0,0 @@
-function cambiarContenido(contenido) {
-    var contents = document.querySelectorAll('.content');
-    for(var i = 0; i < contents.length; i++) {
-        contents[i].style.display = 'none';
-    }
-    document.querySelector('.' + contenido).style.display = 'block';
-}
